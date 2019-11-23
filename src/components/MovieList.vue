8f10b87649cc88bec2b27664ea43919ca95f9b4f
jr31112 <jr435@naver.com>
2019-11-23 16:25:16 +0900

    Vue | Init & Set Router & Main Page

---
+++ src/components/MovieList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="movie-list">
+      <h2>movies</h2>
+      <ul>
+          <li v-for="movie in movies" :key="movie.id">
+              {{ movie.title}}
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name : 'MovieList',
+  props: {
+    movies : {
+      type: Array,
+      required: true
+      }
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <movie-list :movies='movies'/>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+// import router from '../router'
+
+import MovieList from '@/components/MovieList.vue'
+
+export default {
+  name: 'home',
+  components: {
+    MovieList,
+  },
+  data() {
+    return {
+      movies : [],
+    }
+  },
+  methods : {
+    getMovies(){
+      axios.get(`http://127.0.0.1:8000/api/v1/movies/`, this.options)
+    .then(response => {
+      this.movies = response.data
+    })
+    .catch(error => {
+      console.log(error)
+    })
+    }
+  },
+  mounted(){
+    this.getMovies()
+  },
+}
+</script>
