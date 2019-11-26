2d492d9807ad7fff05715bcdbc1f288b4fed93dc
kimjy392 <kimjy392@gmail.com>
2019-11-26 20:01:39 +0900

    영화 목록 돌아가기

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueSession from 'vue-session'
+import Carousel3d from 'vue-carousel-3d';
 
 Vue.config.productionTip = false
 Vue.use(VueSession)
+Vue.use(Carousel3d);
 new Vue({
   router,
   render: h => h(App)
