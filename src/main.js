1c854e2fc258e98c3ab6fe9dd889f8e390df2ab0
kimjy392 <kimjy392@gmail.com>
2019-11-27 11:57:34 +0900

    뷰티파이 추가

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from './App.vue'
 import router from './router'
 import VueSession from 'vue-session'
 import Carousel3d from 'vue-carousel-3d';
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 Vue.use(VueSession)
 Vue.use(Carousel3d);
+
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
